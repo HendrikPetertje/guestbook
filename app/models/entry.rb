76d5c7bf5b9ef2cbe6bf73a6e7ad30c0d6cc6264
picoreclame <peter@picoreclame.nl>
2013-09-26 13:06:21 +0200

    Fix saving strings to database

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,22 +1,5 @@
 class Entry < ActiveRecord::Base
 
-	attr_accessor :name
-	validates_presence_of :name
-
-	attr_accessor :adres
-	validates_presence_of :adres
-
-	attr_accessor :town
-	validates_presence_of :town
-
-	attr_accessor :postbox
-	validates_presence_of :postbox
-
-	attr_accessor :message
-	validates_presence_of :message
-
-
-
-
+validates_presence_of :name, :adres, :town, :postbox, :message
 
 end
