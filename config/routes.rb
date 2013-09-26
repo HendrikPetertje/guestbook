b86b212a8cae933d92c200f74f181277344ea097
picoreclame <peter@picoreclame.nl>
2013-09-26 17:40:51 +0200

    Fix index entry in routes for entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Guestbook::Application.routes.draw do
   resources :users, only: [:index, :update]
 
-  resources :entries, only: [:inex, :new, :edit, :create, :update, :destroy ]
+  resources :entries, only: [:index, :new, :edit, :create, :update, :destroy ]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
