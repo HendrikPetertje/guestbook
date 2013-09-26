d4ecc38143e96a8afa53b6068b98685cd02a7fe3
picoreclame <peter@picoreclame.nl>
2013-09-26 12:56:27 +0200

    Add checks to model The model checks if all fields are filled out now. this will make sure users fill out all required information for the guestbook.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,22 @@
 class Entry < ActiveRecord::Base
+
+	attr_accessor :name
+	validates_presence_of :name
+
+	attr_accessor :adres
+	validates_presence_of :adres
+
+	attr_accessor :town
+	validates_presence_of :town
+
+	attr_accessor :postbox
+	validates_presence_of :postbox
+
+	attr_accessor :message
+	validates_presence_of :message
+
+
+
+
+
 end
